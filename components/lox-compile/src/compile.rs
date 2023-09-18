98cf166f0137b4ed2ee8bccbc216faed69aaf5de
XFFXFF <1247714429@qq.com>
2023-09-18 14:33:27 +0800

    compile calc expression

--- components/lox-compile/src/compile.rs
+++ components/lox-compile/src/compile.rs
@@ -1,7 +1,37 @@
-use lox_ir::{input_file::InputFile, bytecode::Chunk};
+use lox_ir::{input_file::InputFile, bytecode::{Chunk, Code}, syntax};
 
 #[salsa::tracked]
 pub fn compile_file(db: &dyn crate::Db, input_file: InputFile) -> Chunk {
     let exprs = lox_parse::parse_file(db, input_file);
-    todo!()
+    let mut chunk = Chunk::default();
+    for expr in exprs {
+        compile_expr(db, expr, &mut chunk);
+    }
+    chunk
+}
+
+fn compile_expr(db: &dyn crate::Db, expr: &syntax::Expr, chunk: &mut Chunk) {
+    match expr {
+        syntax::Expr::NumberLiteral(word) => {
+            let word_str = word.as_str(db);
+            let value = word_str.parse::<f64>().unwrap();
+            chunk.emit_byte(Code::Constant(value.into()))
+        },
+        syntax::Expr::StringLiteral(_) => todo!(),
+        syntax::Expr::BooleanLiteral(_) => todo!(),
+        syntax::Expr::NilLiteral => todo!(),
+        syntax::Expr::BinaryOp(left, op, right) => {
+            compile_expr(db, &left, chunk);
+            compile_expr(db, &right, chunk);
+            match op {
+                syntax::Op::Plus => chunk.emit_byte(Code::Add),
+                syntax::Op::Minus => chunk.emit_byte(Code::Subtract),
+                syntax::Op::Slash => chunk.emit_byte(Code::Divide),
+                syntax::Op::Star => chunk.emit_byte(Code::Multiply),
+                _ => todo!(),
+            }
+        },
+        syntax::Expr::UnaryOp(_, _) => todo!(),
+        syntax::Expr::Parenthesized(_) => todo!(),
+    }
 }
--- components/lox-ir/src/bytecode.rs
+++ components/lox-ir/src/bytecode.rs
@@ -9,7 +9,13 @@ pub enum Code {
     Divide,
 }
 
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(PartialEq, Eq, Debug, Clone, Default)]
 pub struct Chunk {
     code: Vec<Code>,
+}
+
+impl Chunk {
+    pub fn emit_byte(&mut self, byte: Code) {
+        self.code.push(byte);
+    }
 }
