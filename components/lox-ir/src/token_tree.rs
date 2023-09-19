c7cddb4958f61bc9235db566b70354f75ad2a581
XFFXFF <1247714429@qq.com>
2023-09-19 17:13:50 +0800

    prnit source text of token tree

--- components/lox-ir/src/token_tree.rs
+++ components/lox-ir/src/token_tree.rs
@@ -16,6 +16,7 @@ pub struct TokenTree {
 impl DebugWithDb<dyn crate::Db> for TokenTree {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>, db: &dyn crate::Db, _include_all_fields: bool) -> std::fmt::Result {
         f.debug_struct("TokenTree")
+            .field("source text", &self.input_file(db).source_text(db))
             .field("tokens", &self.tokens(db).debug(db))
             .finish()
     }
