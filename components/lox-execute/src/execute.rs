4c48deaedbfeeccb1fc4a851d20c9b17bf04607b
XFFXFF <1247714429@qq.com>
2023-09-19 08:14:15 +0800

    add Value type

--- components/lox-execute/src/execute.rs
+++ components/lox-execute/src/execute.rs
@@ -1,10 +1,10 @@
 use lox_compile::compile;
 use lox_ir::input_file::InputFile;
 
-use crate::vm::VM;
+use crate::vm::{VM, Value};
 
 // FIXME: This should not return a `f64`, but for now it's convenient as our compiler is more or less a calculator for now.
-pub fn execute_file(db: &impl crate::Db, input_file: InputFile) -> f64 {
+pub fn execute_file(db: &impl crate::Db, input_file: InputFile) -> Value {
     let chunk = compile::compile_file(db, input_file);
     let mut vm = VM::new(chunk);
     vm.interpret()
--- components/lox-execute/src/vm.rs
+++ components/lox-execute/src/vm.rs
@@ -1,11 +1,74 @@
 use lox_ir::bytecode;
 
+#[derive(Debug, Clone, Copy)]
+pub enum Value {
+    Number(f64),
+    Boolean(bool),
+    Nil,
+}
+
+impl From<f64> for Value {
+    fn from(num: f64) -> Self {
+        Value::Number(num)
+    }
+}
+
+impl From<bool> for Value {
+    fn from(b: bool) -> Self {
+        Value::Boolean(b)
+    }
+}
+
+impl std::ops::Add for Value {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a + b),
+            _ => panic!("Cannot add {:?} and {:?}", self, rhs),
+        }
+    }
+}
+
+impl std::ops::Sub for Value {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a - b),
+            _ => panic!("Cannot subtract {:?} and {:?}", self, rhs),
+        }
+    }
+}
+
+impl std::ops::Mul for Value {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a * b),
+            _ => panic!("Cannot multiply {:?} and {:?}", self, rhs),
+        }
+    }
+}
+
+impl std::ops::Div for Value {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Value::Number(a), Value::Number(b)) => Value::Number(a / b),
+            _ => panic!("Cannot divide {:?} and {:?}", self, rhs),
+        }
+    }
+}
+
 pub struct VM {
     chunk: bytecode::Chunk,
     ip: usize,
 
     // FIXME: This should be a stack of values, not a stack of f64s.
-    stack: Vec<f64>,
+    stack: Vec<Value>,
 }
 
 impl VM {
@@ -19,7 +82,7 @@ impl VM {
 
     // FIXME: `interpret` should not return a `f64`, but for now it's convenient as
     //  our compiler is more or less a calculator.
-    pub fn interpret(&mut self) -> f64 {
+    pub fn interpret(&mut self) -> Value {
         loop {
             if self.chunk.len() <= self.ip {
                 break;
@@ -29,7 +92,7 @@ impl VM {
             let instruction = self.read_byte();
             match instruction {
                 bytecode::Code::Return => break,
-                bytecode::Code::Constant(value) => self.push(value.into()),
+                bytecode::Code::Constant(value) => self.push(value.0),
                 bytecode::Code::Add => {
                     let b = self.pop();
                     let a = self.pop();
@@ -69,11 +132,11 @@ impl VM {
         byte
     }
 
-    fn pop(&mut self) -> f64 {
+    fn pop(&mut self) -> Value {
         self.stack.pop().unwrap()
     }
 
-    fn push(&mut self, value: f64) {
-        self.stack.push(value);
+    fn push<T>(&mut self, value: T) where T: Into<Value> {
+        self.stack.push(value.into());
     }
 }
