a3371499d78c47a1401dfd42e943e0a43e584a14
XFFXFF <1247714429@qq.com>
2023-11-04 20:03:23 +0800

    try codemirror

--- components/lox-web/js/index.js
+++ components/lox-web/js/index.js
@@ -1,1 +1,16 @@
 import("../pkg/index.js").catch(console.error);
+import { EditorState } from "@codemirror/state"
+import { EditorView, keymap } from "@codemirror/view"
+import { defaultKeymap } from "@codemirror/commands"
+import { basicSetup } from "codemirror"
+
+let startState = EditorState.create({
+    doc: "Hello World",
+    extensions: [basicSetup, keymap.of(defaultKeymap)]
+})
+
+let view = new EditorView({
+    state: startState,
+    parent: document.body
+})
+
